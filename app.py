18e27195e4aa746fa2fbcd35c97225ad5c71e2b8
Aman Shahzad <158335405+AmanShahzad1@users.noreply.github.com>
2026-04-17 00:08:42 +0500

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import glob
 from langchain_groq import ChatGroq
 from sentence_transformers import SentenceTransformer  # New embedding model
 from langchain_huggingface import HuggingFaceEmbeddings  # Wrapper for SentenceTransformer
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains import create_retrieval_chain
